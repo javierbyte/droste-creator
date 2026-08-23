b3d5aa74b0ce795cd26ac8a2fbe98312a7e56c24
javierbyte <hi@javier.xyz>
2026-08-23 18:31:58 +0200

    Update dependencies and improve GitHub corner styling

--- src/components/GitHubCorner.jsx
+++ src/components/GitHubCorner.jsx
@@ -1,7 +1,6 @@
 export default function GitHubCorner() {
   return (
     <a
-      style={{ zIndex: 1, position: 'fixed', top: 0, right: 0 }}
       href="https://github.com/javierbyte/droste-creator"
       aria-label="github:@javierbyte"
       className="github-corner"
